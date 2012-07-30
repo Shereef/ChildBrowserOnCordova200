46924b6064497146b35c4a7fdc66702b6ca5a427
Shereef Marzouk <Shereef@aofar.ly>
2012-07-30 14:34:13 +0200

    Added ChildBrowser.install() this is usually added in onDeviceReady in your app, but since this default template already has one i had to use it

--- www/js/index.js
+++ www/js/index.js
@@ -9,8 +9,12 @@ var app = {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
         app.report('deviceready');
+        app.report('Installing child browser');
+        //ChildBorwser init
+        cb = ChildBrowser.install();
+        app.report('installed child browser'); // for some reason this doesn't run
     },
-    report: function(id) { 
+    report: function(id) {
         console.log("report:" + id);
         // hide the .pending <p> and show the .complete <p>
         document.querySelector('#' + id + ' .pending').className += ' hide';
